5a1741a8c502888c618a7194cce6aaccb478c4e9
vik-kalsi <107356027+vik-kalsi@users.noreply.github.com>
2023-08-29 22:55:24 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+let  Q1Budapest = document.getElementById("Q1Budapest");
+let Q1Bratislava = document.getElementById("Q1Belgrade")
+let Q1Prague = document.getElementById("Q1Prague")
+let Q1Belgrade = document.getElementById("Q1Belgrade")
+
+let Q1IncorrectAnswers = [Q1Belgrade, Q1Prague, Q1Belgrade];
+let score = 0;
+
+
+
+
+Q1Budapest.addEventListener("click", function() {
+    
+})
+
